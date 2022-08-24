f243e7aa9b1d2778c9ac877f05f4a475df95ef7b
Mehmet eren Akbulut <makbulut@k1m29s04.42istanbul.com.tr>
2022-08-24 21:26:15 +0300

    Denemek için bir kaç bir şey yazdım

---
+++ ex01/Phonebook.hpp
@@ -0,0 +1,27 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+class Phonebook
+{
+private:
+    /* data */
+public:
+    Phonebook(/* args */);
+    ~Phonebook();
+    void    EXIT();
+};
+
+Phonebook::Phonebook()
+{
+
+}
+
+Phonebook::~Phonebook()
+{
+}
+
+void    Phonebook::EXIT(){
+
+}
+
+#endif
