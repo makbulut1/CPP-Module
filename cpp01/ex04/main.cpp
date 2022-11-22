e45b98665aef2b4b43fb5a3422f55da84a82e9ba
Mehmet eren Akbulut <makbulut@k1m41s05.42istanbul.com.tr>
2022-11-22 21:47:07 +0300

    ex04 yapılıyor

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,20 +1,35 @@
 #include "iostream"
-
-void    replace(std::string s1, std::string s2, std::string s3){
-    std::size_t found = s1.find(s2);
-    if (found != s1.npos)
-        std::cout << found << std::endl;
-    (void)s3;
-}
+#include "fstream"
+//void    replace(std::string s1, std::string s2, std::string s3){
+//    std::size_t found = -1;
+//
+//    label:
+//    found = s1.find(s2, found + 1);
+//    for (int j = 0; s2[j] || s3[j]; j++){
+//        s1[found] = s3[j];
+//        found++;
+//    }
+//    if (found < s1.length())
+//      goto label;
+//    std::cout << s1 << std::endl;
+//}
 
 int main(int ac, char **av)
 {
+    (void)av;
     if (ac == 4)
     {
-        const std::string &s1(av[1]);
-        const std::string &s2(av[2]);
-        const std::string  &s3(av[3]);
-        replace(s1, s2, s3);
+      std::string  fileName = av[1];
+        std::fstream file(fileName);
+        fileName = fileName.substr(0, fileName.find_last_of(".")) + ".replace";
+        std::ofstream newFile(fileName);
+        std::cout << fileName;
+//        const std::string s1(av[1]);
+//        const std::string s2(av[2]);
+//        const std::string  s3(av[3]);
+//        replace(s1, s2, s3);
+//        replace(&s1, &s2, &s3);
+//        replace(&s1, &s2, &s3);
     } else
         std::cout << "Error: " << std::endl;
     return 0;
