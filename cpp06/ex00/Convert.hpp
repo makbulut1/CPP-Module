9373b640e87f7b35c8dfabae56c3a379378ef825
Mehmet eren Akbulut <makbulut@student.42istanbul.com.tr>
2022-12-14 13:52:33 +0300

    cpp06 started. I will change computer.

---
+++ cpp06/ex00/Convert.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Mehmet eren Akbulut on 12/14/22.
+//
+
+#ifndef CPP_MODULE_CONVERT_HPP
+#define CPP_MODULE_CONVERT_HPP
+
+
+class Convert {
+
+};
+
+
+#endif //CPP_MODULE_CONVERT_HPP
---
+++ cpp06/ex00/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Mehmet eren Akbulut on 12/14/22.
+//
+#include <iostream>
+
+int main(int ac, char **av){
+    (void)ac;
+    (void)av;
+    return 0;
+}
