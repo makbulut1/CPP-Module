c13b214857283e1d624755c7982702600ad8267d
Mehmet eren Akbulut <makbulut@student.42istanbul.com.tr>
2022-12-14 23:18:04 +0300

    cpp06 ex01 done

--- cpp06/ex01/Data.cpp
+++ cpp06/ex01/Data.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Data.hpp"
+
+uintptr_t   serialize(Data* ptr){
+    uintptr_t a = reinterpret_cast<uintptr_t>(ptr);
+    return a;
+}
+Data*        deserialize(uintptr_t raw){
+    Data *a = reinterpret_cast<Data*>(raw);
+    return a;
+}
--- cpp06/ex01/Data.hpp
+++ cpp06/ex01/Data.hpp
@@ -5,12 +5,18 @@
 #ifndef CPP_MODULE_DATA_HPP
 #define CPP_MODULE_DATA_HPP
 
+#include "iostream"
 
+typedef struct  s_Data{
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    int		    age;
+    double	    level;
+}               Data;
 
-class Data {
+uintptr_t   serialize(Data* ptr);
 
-};
-
-
+Data*        deserialize(uintptr_t raw);
 
 #endif //CPP_MODULE_DATA_HPP
